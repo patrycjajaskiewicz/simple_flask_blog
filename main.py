6ed7d2f59eee0155bd585155fd8f266f308d87f4
patrycjajaskiewicz <patrycja.kowalska642@gmail.com>
2023-03-26 18:04:14 +0100

    upgraded flask blog

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
-from flask import Flask, render_template
-import pip._vendor.requests 
+from flask import Flask,render_template
+import pip._vendor.requests
 
 
 app = Flask(__name__)
-
-# requesting api with all blogs and saving it as a json 
+#requesting blogs api and saving it as a json data
 request = pip._vendor.requests
 respond = request.get('https://api.npoint.io/c790b4d5cab58020d391')
-blogs = respond.json()
+json_data = respond.json()
 
-
-#route to main page with all blogs title and subtitle
 @app.route('/')
 def home():
-    return render_template("index.html", all_blogs=blogs)
+    return render_template('index.html', json_data=json_data)
 
 
-#route to separate blog post with title and body text
-#using number variable(contain post id) created in index.html to create specific blog page
-@app.route('/post/<int:number>')
-def post(number):
-        #looping through all blog posts and checking if blog id is indentical to number variable 
-        for blog in blogs: 
-            if blog['id'] == number:
-                return render_template('post.html', all_blogs=blogs, number=number)
-                    
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-if __name__ == "__main__":
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/post/<int:num>')
+def post(num):
+    for blog in json_data:
+        if blog['id'] == num:
+            return render_template('post.html', json_data=json_data, num=num)
+
+
+
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
